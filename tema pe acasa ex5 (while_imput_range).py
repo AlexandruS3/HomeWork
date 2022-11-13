89a9eeddbb9067de3758300b3ced4b5997c6af8b
AlexandruS3 <117845597+AlexandruS3@users.noreply.github.com>
2022-11-13 19:44:13 +0200

    Add files via upload

---
+++ tema pe acasa ex5 (while_imput_range).py
@@ -0,0 +1,36 @@
+from os import system
+
+system ('cls')
+start_n = int(input ('introdu un numar de la 1 pina la 10 '))
+end_n   = int(input ('introdu un numar de la 1 pina la 10 cu exceptia celui deja introdus '))
+n = 0
+n2= 10
+while n <= 10:
+    
+    if end_n >= n and start_n <= n:
+     print (n)
+    n +=1
+    if end_n <= n2 and start_n >= n2:
+     print (n2)
+    n2 -=1
+
+    
+ 
+    
+    
+        
+
+
+   
+     
+    
+    
+########################################################    
+    
+
+
+    
+    
+
+
+    
